0deaeff5fca41577e9df22495b9aa405a2865612
chitra0404 <chitrasuresh0404@gmail.com>
2024-01-04 22:01:33 +0530

    commit

---
+++ front/src/App.jsx
@@ -0,0 +1,29 @@
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import AccountActivation from "./components/AccountActivation";
+import ForgotPassword from "./components/ForgotPassword";
+import UpdatePassword from "./components/UpdatePassword";
+import Urlshortener from "./components/Urlshortener";
+
+function App() {
+  const url = "https://url-shortener-backend-tt62.onrender.com";
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login url={url} />}></Route>
+          <Route path='/UrlShortener/Signup' element={<Signup url={url}/>} ></Route>
+          <Route path="/UrlShortener/account_activation/:id" element={<AccountActivation url={url} />} ></Route>
+          <Route path="/UrlShortener/forgotPassword" element={<ForgotPassword url={url}/>}></Route>
+          <Route path="/UrlShortener/UpdatePassword/:id" element={<UpdatePassword url={url}/>}></Route>
+          <Route path="/UrlShortener" element={<Urlshortener url={url} />}></Route>
+                    
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
